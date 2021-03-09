531c9b062cf65e3034f6f516ac24f62e6eacb1b2
BCsabaEngine <csaba@balazs-magyar.hu>
2021-03-09 14:29:54 +0100

    Numeric edit

---
+++ TerminalNumEdit.cpp
@@ -0,0 +1,101 @@
+#include "TerminalConfig.h"
+#include "TerminalNumEdit.h"
+
+TerminalNumEdit::TerminalNumEdit(byte x, byte y, byte width): TerminalControl(x, y, width) {
+}
+
+int TerminalNumEdit::getValue() {
+  return this->value;
+}
+
+void TerminalNumEdit::incValue(int increment) {
+  this->setValue(this->getValue() + increment);
+}
+
+void TerminalNumEdit::setValue(int value) {
+  this->value = value;
+  if (this->value > this->max)
+    this->value = this->max;
+  if (this->value < this->min)
+    this->value = this->min;
+
+  if (this->changehandler)
+    this->changehandler(this, value);
+
+  this->redrawScreen();
+}
+
+void TerminalNumEdit::setMinMax(int min, int max) {
+  if (min > max)
+    min = max - 1;
+  this->min = min;
+  this->max = max;
+
+  this->setValue(this->getValue());
+}
+
+void TerminalNumEdit::draw(BasicTerm* term, bool focused) {
+  this->labelDraw(term);
+
+  String text = String(this->value);
+  while (text.length() < this->width)
+    text = " " + text;
+  text = "(" + text + ")";
+
+  if (focused)
+    term->set_attribute(BT_REVERSE);
+
+  term->position(this->y, this->x);
+  term->print(text);
+
+  term->set_attribute(BT_NORMAL);
+}
+
+bool TerminalNumEdit::handleKey(uint16_t key) {
+  switch (key) {
+
+#ifdef CONTROL_EDIT_UP_DOWN
+    case BT_KEY_UP:
+      this->incValue(1);
+      return true;
+#endif
+
+#ifdef CONTROL_EDIT_LEFT_RIGHT
+    case BT_KEY_RIGHT:
+      this->incValue(1);
+      return true;
+#endif
+
+    case 0x2b: // numeric plus
+      this->incValue(1);
+      return true;
+
+#ifdef CONTROL_EDIT_UP_DOWN
+    case BT_KEY_DOWN:
+      this->incValue(-1);
+      return true;
+#endif
+
+#ifdef CONTROL_EDIT_LEFT_RIGHT
+    case BT_KEY_LEFT:
+      this->incValue(-1);
+      return true;
+#endif
+
+    case 0x2d: // numeric minus
+      this->incValue(-1);
+      return true;
+
+  }
+
+  if (key >= 0x30 && key <= 0x39)
+  {
+    int value = this->getValue();
+    if (millis() - this->lastKey > CONTROL_EDIT_MAXLATENCY_MS)
+      value = 0;
+    this->setValue(value * 10 + key - 0x30);
+
+    this->lastKey = millis();
+  }
+  return false;
+}
---
+++ TerminalNumEdit.h
@@ -0,0 +1,32 @@
+#ifndef TerminalNumEdit_h
+#define TerminalNumEdit_h
+
+#include <BasicTerm.h>
+#include "TerminalControl.h"
+
+class TerminalNumEdit;
+typedef void (*numeditFuncPtr)(TerminalNumEdit *numedit, int value);
+class TerminalNumEdit: public TerminalControl {
+  private:
+    int value = 0;
+    int min = 0;
+    int max = 1000;
+    unsigned long lastKey = 0;
+    numeditFuncPtr changehandler = NULL;
+  public:
+    TerminalNumEdit(byte x, byte y, byte width);
+    int getValue();
+    void incValue(int increment = 1);
+    void setValue(int value);
+    void setMinMax(int min, int max);
+    virtual void draw(BasicTerm* term, bool focused);
+    virtual bool canFocus() {
+      return true;
+    }
+    virtual bool handleKey(uint16_t key);
+    void setChangeHandler(numeditFuncPtr changehandler) {
+      this->changehandler = changehandler;
+    }
+};
+
+#endif
