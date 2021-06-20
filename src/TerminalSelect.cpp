3e81dad434bd5e286f69f323bf5d11984b7ff951
BCsabaEngine <csaba@balazs-magyar.hu>
2021-06-20 20:18:32 +0200

    Select (lookup) control

---
+++ src/TerminalSelect.cpp
@@ -0,0 +1,124 @@
+#include "TerminalConfig.h"
+#include "TerminalSelect.h"
+
+TerminalSelect::TerminalSelect(byte x, byte y, byte width) : TerminalControl(x, y, width) {}
+
+void TerminalSelect::setItems(String items[], int itemcount)
+{
+  this->itemcount = itemcount;
+  for (int i = 0; i < itemcount; i++)
+    this->items[i] = items[i];
+}
+
+int TerminalSelect::getIndex()
+{
+  return this->index;
+}
+
+void TerminalSelect::setIndex(int index, bool canunderoverflow)
+{
+  this->index = index;
+  if (canunderoverflow)
+  {
+    while (this->index > this->itemcount - 1)
+      this->index = 0;
+    if (this->index < 0)
+      this->index = this->itemcount - 1;
+  }
+  else
+  {
+    if (this->index > this->itemcount - 1)
+      this->index = this->itemcount - 1;
+    if (this->index < 0)
+      this->index = 0;
+  }
+
+  if (this->changehandler)
+    this->changehandler(this, this->index);
+
+  this->redrawScreen();
+}
+
+void TerminalSelect::setPrev()
+{
+  this->setIndex(this->getIndex() - 1);
+}
+
+void TerminalSelect::setNext(bool canoverflow)
+{
+  this->setIndex(this->getIndex() + 1, canoverflow);
+}
+
+void TerminalSelect::draw(BasicTerm *term, bool focused)
+{
+  if (!this->visible)
+    return;
+
+  this->labelDraw(term);
+
+  String text = this->items[this->index];
+  while (text.length() < this->width)
+    text += " ";
+  text = "<" + text + ">";
+
+  if (focused)
+    term->set_attribute(BT_REVERSE);
+
+  term->position(this->y, this->x);
+  term->print(text);
+
+  term->set_attribute(BT_NORMAL);
+}
+
+bool TerminalSelect::handleKey(uint16_t key)
+{
+  switch (key)
+  {
+
+#ifdef CONTROL_CHECKBOX_SPACE
+  case ' ':
+    this->setNext(true);
+    return true;
+#endif
+
+#ifdef CONTROL_CHECKBOX_ENTER
+  case 0xA:
+    this->setNext(true);
+    return true;
+#endif
+
+#ifdef CONTROL_EDIT_UP_DOWN
+  case BT_KEY_UP:
+    this->setNext();
+    return true;
+#endif
+
+#ifdef CONTROL_EDIT_LEFT_RIGHT
+  case BT_KEY_RIGHT:
+    this->setNext();
+    return true;
+#endif
+
+  case 0x2b: // numeric plus
+    this->setNext();
+    return true;
+
+#ifdef CONTROL_EDIT_UP_DOWN
+  case BT_KEY_DOWN:
+    this->setPrev();
+    return true;
+#endif
+
+#ifdef CONTROL_EDIT_LEFT_RIGHT
+  case BT_KEY_LEFT:
+    this->setPrev();
+    return true;
+#endif
+
+  case 0x2d: // numeric minus
+    this->setPrev();
+    return true;
+  }
+
+  return false;
+}
---
+++ src/TerminalSelect.h
@@ -0,0 +1,32 @@
+#ifndef TerminalSelect_h
+#define TerminalSelect_h
+
+#include <string.h>
+#include "lib/BasicTerm.h"
+#include "TerminalControl.h"
+
+class TerminalSelect;
+typedef void (*selectFuncPtr)(TerminalSelect *select, int index);
+class TerminalSelect : public TerminalControl
+{
+private:
+  int itemcount;
+  String items[CONTROL_SELECT_MAX_ITEMCOUNT];
+  int index = 0;
+  selectFuncPtr changehandler = NULL;
+
+public:
+  TerminalSelect(byte x, byte y, byte width);
+  virtual ~TerminalSelect() {}
+  void setItems(String items[], int itemcount);
+  int getIndex();
+  void setIndex(int index, bool canunderoverflow = false);
+  void setPrev();
+  void setNext(bool canoverflow = false);
+  virtual void draw(BasicTerm *term, bool focused);
+  virtual bool canFocus() { return this->visible; }
+  virtual bool handleKey(uint16_t key);
+  void setChangeHandler(selectFuncPtr changehandler) { this->changehandler = changehandler; }
+};
+
+#endif
